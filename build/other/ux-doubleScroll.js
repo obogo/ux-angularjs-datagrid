b528b679196c61cb6857d88e8880153b166024b4
Wes Jones <cybus10@gmail.com>
2014-01-02 10:25:11 -0700

    added double scroll. Still a bug to fix. fixed unit tests.

---
+++ build/other/ux-doubleScroll.js
@@ -0,0 +1,83 @@
+/*
+* uxDatagrid v.0.1.0
+* (c) 2014, WebUX
+* License: MIT.
+*/
+(function(exports, global){
+angular.module("ux").directive("uxDoubleScroll", function() {
+    return {
+        link: function(scope, element, attr) {
+            var el = element[0], lastValue = 0, selector = scope.$eval(attr.uxDoubleScroll), target = document.querySelector(selector), isIOS = navigator.userAgent.match(/(iPad|iPhone|iPod)/g), vScroll, contentHeight = 0, elHeight = 0, scrollModel = scope.datagrid && scope.datagrid.scrollModel || {};
+            function onScroll(event) {
+                if (target) {
+                    if (el.scrollTop + el.offsetHeight <= el.scrollHeight) {
+                        scope.outerScrollTop = "hidden " + el.scrollTop + "/" + (el.scrollTop + el.offsetHeight) + "/" + el.scrollHeight;
+                        target.style.overflow = "hidden";
+                    } else {
+                        scope.outerScrollTop = "scroll " + el.scrollTop + "/" + (el.scrollTop + el.offsetHeight) + "/" + el.scrollHeight;
+                        target.style.overflow = "scroll";
+                    }
+                } else {
+                    throw new Error("double scroll requires a selector.");
+                }
+            }
+            function onIOScroll(value) {
+                if (vScroll.enabled && value !== lastValue) {
+                    lastValue = value;
+                    vScroll.content.css({
+                        webkitTransform: "translate3d(0px, " + (element[0].scrollTop - value) + "px, 0px)"
+                    });
+                }
+            }
+            function onSizeChange() {
+                var content = element.children();
+                elHeight = element[0].offsetHeight;
+                target.style.height = elHeight + "px";
+                content.children()[1].style.height = elHeight + "px";
+                contentHeight = content.children()[0].offsetHeight + elHeight;
+                content[0].style.height = contentHeight + "px";
+                scope.datagrid.updateViewportHeight();
+            }
+            function onTargetScrollToTop(event, scroller, speed) {
+                if (scroller.element[0] === target) {
+                    scrollModel.enable(false);
+                    vScroll.enable(true, speed);
+                    target.disabled = "disabled";
+                }
+            }
+            function onDoubleScrollBottom(event, scroller, speed) {
+                if (scroller.element[0] === element[0]) {
+                    vScroll.enable(false);
+                    scrollModel.enable(true, vScroll.getValues().speed);
+                    target.disabled = "";
+                }
+            }
+            if (isIOS) {
+                vScroll = ux.datagrid.VirtualScroll(scope, element, {}, onIOScroll);
+                vScroll.setup();
+                onSizeChange();
+                onIOScroll(0);
+                scope.$on(exports.datagrid.events.READY, function() {
+                    onSizeChange();
+                    onTargetScrollToTop(null, scrollModel, 0);
+                });
+                scope.$on(exports.datagrid.events.VIRTUAL_SCROLL_TOP, onTargetScrollToTop);
+                scope.$on(exports.datagrid.events.VIRTUAL_SCROLL_BOTTOM, onDoubleScrollBottom);
+            } else {
+                element[0].addEventListener("scroll", onScroll, true);
+                onScroll(null);
+                scope.$on(exports.datagrid.events.READY, onSizeChange);
+            }
+            scope.$on(exports.datagrid.events.RESIZE, onSizeChange);
+            scope.$on("$destroy", function() {
+                if (isIOS) {
+                    vScroll.destroy();
+                    vScroll = null;
+                } else {
+                    element[0].removeEventListener("scroll", onScroll);
+                }
+            });
+        }
+    };
+});
+}(this.ux = this.ux || {}, function() {return this;}()));

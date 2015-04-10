78056eddf53cdda265a2f8ee454969ef9cf3d99e
wesjones <cybus10@gmail.com>
2015-04-10 13:23:07 -0600

    update to fix expanding rows bug if expanding rows are bigger than viewable area.

--- build/latest/angular-ux-datagrid.js
+++ build/latest/angular-ux-datagrid.js
@@ -1759,7 +1759,7 @@ function Datagrid(scope, element, attr, $compile) {
         result.startIndex = result.i = inst.getOffsetIndex(scroll);
         if (inst.rowsLength && result.startIndex === result.end) {
             result.startIndex = result.end - 1;
-            throw new Error(exports.errors.E1002);
+            inst.log(exports.errors.E1002);
         }
         return result;
     }
